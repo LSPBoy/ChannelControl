437617958f12dc67b8fbcb8958f3bf96b73060a4
lishaopeng <2259288066@qq.com>
2018-12-03 22:28:28 +0800

    no message

---
+++ ChannelDemo/ChannelDemo/ViewController.m
@@ -0,0 +1,42 @@
+//
+//  ViewController.m
+//  ChannelDemo
+//
+//  Created by Object on 2018/12/3.
+//  Copyright © 2018年 lsp. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "ChannelControl.h"
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.title = @"ChannelControl";
+    self.view.backgroundColor = [UIColor whiteColor];
+    self.navigationController.navigationBar.tintColor = [UIColor blackColor];
+    
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(showChannel)];
+}
+
+-(void)showChannel
+{
+    NSArray *arr1 = @[@"要闻",@"河北",@"财经",@"娱乐",@"体育",@"社会",@"NBA",@"视频",@"汽车",@"图片",@"科技",@"军事",@"国际",@"数码",@"星座",@"电影",@"时尚",@"文化",@"游戏",@"教育",@"动漫",@"政务",@"纪录片",@"房产",@"佛学",@"股票",@"理财"];
+    NSArray *arr2 = @[@"有声",@"家居",@"电竞",@"美容",@"电视剧",@"搏击",@"健康",@"摄影",@"生活",@"旅游",@"韩流",@"探索",@"综艺",@"美食",@"育儿"];
+    [[ChannelControl shareControl] showChannelViewWithInUseTitles:arr1 unUseTitles:arr2 finish:^(NSArray *inUseTitles, NSArray *unUseTitles) {
+        NSLog(@"inUseTitles = %@",inUseTitles);
+        NSLog(@"unUseTitles = %@",unUseTitles);
+    }];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
